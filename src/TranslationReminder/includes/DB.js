b9ea19743d2cbe9711642c2776d3a99892ff267b
Alexey Skrypnik <alexey.skrypnik@wonga.com>
2013-06-10 14:55:18 +0100

    IDE was switched to VS2010. Further code cleaning and improving.

---
+++ src/TranslationReminder/includes/DB.js
@@ -0,0 +1,183 @@
+var DB = function ()
+{
+	function getDb()
+	{
+		opera.postError("Windows is: " + window);
+		var db = window.openDatabase('YMDB', '1.0', 'Translation Reminder', 2 * 1024 * 1024);
+		db.transaction(function (tx)
+		{
+			tx.executeSql('CREATE TABLE IF NOT EXISTS words (word, meaning, toDelete, toEdit, userName, date)', null, null,
+			 function (tx, error)
+			 {
+		 		opera.postError("Error: " + error);
+			 });
+		});
+
+		return db;
+	};
+
+
+	this.SaveUserID = function (userId)
+	{
+		localStorage.setItem("userId", userId);
+	};
+
+	this.GetUserID = function ()
+	{
+		var userId = null;
+		try
+		{
+			userId = localStorage.getItem("userId");
+		}
+		catch (ex)
+		{
+			opera.postError("User ID Access error: " + ex.toString());
+		}
+
+		return userId;
+	};
+
+
+	this.GetWords = function (backMessage, event, callback)
+	{
+		// TODO: call synchronize explicitly
+		var userName = this.GetUserID();
+		if (!userName)
+			return false;
+
+		var db = getDb();
+		opera.postError("DB: " + db);
+		db.transaction(function (tx)
+		{
+			opera.postError("TX: " + tx);
+			var toDelete = "toDelete = 0";
+			if (callback)
+			{
+				toDelete = "1";
+			}
+			tx.executeSql('SELECT * FROM words WHERE userName =? AND ' + toDelete + " ORDER BY date DESC", [userName],
+			function (tx, results)
+			{
+				opera.postError("Result length: " + results.rows.length);
+				var callbackArray = new Array();
+				for (var i = 0; i < results.rows.length; i++)
+				{
+					callbackArray.push
+						({
+						word: results.rows.item(i).word,
+						meaning: results.rows.item(i).meaning,
+						toDelete: results.rows.item(i).toDelete,
+						toEdit: results.rows.item(i).toEdit,
+						date: results.rows.item(i).date
+						});
+				}
+				if (callback)
+				{
+					event.source.postMessage({ message: "begin_synchronize", words: callbackArray });
+				} else
+				{
+					event.source.postMessage({ message: backMessage, data: callbackArray });
+				}
+			},
+			 function (tx, error)
+			 {
+		 		opera.postError("Get error: " + error);
+			 });
+
+			return true;
+		});
+	};
+
+
+	this.WriteWord = function (word, meaning, event, date)
+	{
+		var userName = this.GetUserID();
+		if (!userName)
+		{
+			event.source.postMessage({ message: "no_user" });
+			return false;
+		}
+
+		word = fullTrim(word);
+		opera.postError("Write data: " + word + meaning + event);
+		var db = getDb();
+		db.transaction(function (tx)
+		{
+			if (date === undefined || date === null)
+			{
+				date = new Date().getTime();
+			}
+
+			date = parseInt(date);
+			tx.executeSql('INSERT INTO words (word, meaning, toDelete, toEdit, userName, date) ' +
+			'VALUES (?, ?, 0, 0, ?, ?)', [word.toLowerCase(), meaning, userName, date],
+			function (tx, results)
+			{
+				if (event)
+				{
+					event.source.postMessage({ message: "writed" });
+				}
+			},
+			function (tx, error)
+			{
+				opera.postError("Write error: " + error);
+			});
+		});
+
+		return true;
+	};
+
+
+	this.DeleteWord = function (word, event)
+	{
+		var userName = this.GetUserID();
+		if (!userName)
+			return false;
+
+		word = fullTrim(word);
+		var db = getDb();
+		db.transaction(function (tx)
+		{
+			tx.executeSql('UPDATE words SET toDelete=1 WHERE (word)=? AND (userName)=?', [word.toString().toLowerCase(), userName],
+			function (tx, results)
+			{
+				if (event)
+				{
+					event.source.postMessage({ message: "deleted", data: word });
+				}
+			},
+			function (tx, error)
+			{
+				opera.postError("Delete error: " + error);
+			});
+		});
+
+		return true;
+	};
+
+	this.DeleteAllWords = function (event)
+	{
+		var userName = this.GetUserID();
+		if (!userName)
+			return false;
+
+		var db = getDb();
+		db.transaction(function (tx)
+		{
+			tx.executeSql('DELETE FROM words WHERE (userName) = ?', [userName],
+			function (tx, results)
+			{
+				if (event)
+				{
+					event.source.postMessage({ message: "deleted", data: word });
+				}
+			},
+			function (tx, error)
+			{
+				opera.postError("Delete error: " + error);
+			});
+		});
+
+		return true;
+	};
+};
---
+++ src/TranslationReminder/includes/foreground.js
@@ -0,0 +1,102 @@
+var frontend = new Frontend();
+
+opera.extension.onmessage = function(event) 
+{
+// Get content of incoming message.
+var message = event.data.message;
+var data = event.data.data;
+
+switch (message)
+{
+    case "loaded":
+	window.addEventListener("DOMContentLoaded", function()
+	{
+	document.onmouseup = selectWord;
+	refreshPage();
+	}, false);
+	break;
+
+    case "no_user":
+	alert("First Log In or create New ID");
+	break;
+
+    case  "writed":
+	frontend.refreshPage();
+	break;
+
+    case  "readed_on_page":
+	frontend.refreshCallback((data));
+	break;
+
+    case  "readed_on_table":
+	frontend.reloadTableCallback((data));
+	break;
+
+    case  "deleted":
+	frontend.removeHighLights(data);
+	frontend.refreshPage();
+	break;
+
+    case "begin_synchronize":
+	opera.extension.postMessage({action: "synchronize", words: event.data.words});
+	break;
+
+    case "synchronized":
+	frontend.dataSynchronized(data);
+	frontend.refreshPage();
+	break;
+
+    case "logined":
+	frontend.backToMain();
+	frontend.synchonizeData();
+	break;
+
+    case "login_failed":
+	alert("Cound not login. UserId does not exists");
+	document.getElementById("_loading_view").style.display = "none";
+	break;
+
+    case "acc_created":
+	frontend.backToMain();
+	frontend.synchonizeData();
+	break;
+
+    case "acc_created_failed":
+	alert("Cound not create account. Try another");
+	document.getElementById("_loading_view").style.display = "none";
+	break;
+
+    case "show_username":
+	showUserName(data);
+	frontend.break;
+
+    case "paste_username":
+	frontend.pasteUserName(data);
+	break;
+}
+};
+
+
+// on load function for testing out of widget
+/*window.addEventListener("DOMContentLoaded", function()
+{
+   document.onmouseup = selectWord;
+   //document.body.onclick = showWordsTable();
+   refreshPage();
+
+   var displayer = document.createElement("div");
+   displayer.appendChild(document.createTextNode("SHOW"));
+   displayer.style.position = "fixed";
+   displayer.style.display = "table";
+   displayer.style.background = "#aaa";
+   displayer.style.left = "0px";
+   displayer.style.top = "0px";
+
+
+   document.body.appendChild(displayer);
+   displayer.onclick = showWordsTable;
+
+}, false);*/
+
+
+
